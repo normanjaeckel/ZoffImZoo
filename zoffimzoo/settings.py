d31eefe213a0eebe751ee70a25e90460aa7b0449
Norman Jäckel <contact@normanjaeckel.de>
2020-04-17 14:13:52 +0200

    Changed settings.

--- zoffimzoo/settings.py
+++ zoffimzoo/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["zoffimzoo.normanjaeckel.de"]
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "deployment", "static")
